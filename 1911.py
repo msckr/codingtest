faa3663aaeaabfc5d23ca74f9857939b00083ac8
Minsoo Jo <mschoyb@naver.com>
2023-04-07 17:45:39 +0900

    1911, 흙길 보수하기 indexerror

--- 1911.py
+++ 1911.py
@@ -1,2 +1,18 @@
 N,L = map(int,input().split())
-print(N,L)
+lake = [list(map(int,input().split())) for _ in range(N)]
+lake.sort()
+num = 0
+for pool in range(len(lake)):
+    l = lake[pool][1] - lake[pool][0]
+    if pool == len(lake) + 1:
+        num+=(l-1)/L+1
+        break
+    if (l%L):
+        tmp = L -((l)%L)
+        now = lake[pool][1]+tmp
+        if lake[pool+1][0] <= now:
+            lake[pool+1][0] = now
+        num += (l)//L+1
+    else:
+        num += l/L
+print(int(num))
