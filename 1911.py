d701807a7a0f14000d000996c6de23197b63f444
Minsoo Jo <mschoyb@naver.com>
2023-04-10 15:46:36 +0900

    1911 흙길 보수하기

--- 1911.py
+++ 1911.py
@@ -1,18 +1,33 @@
-N,L = map(int,input().split())
-lake = [list(map(int,input().split())) for _ in range(N)]
+import sys
+sys.setrecursionlimit(1000000)
+N,L = map(int,sys.stdin.readline().split())
+lake = []
+for _ in range(N):
+    lake.append(list(map(int, sys.stdin.readline().split())))
 lake.sort()
-num = 0
+cnt = 0
 for pool in range(len(lake)):
-    l = lake[pool][1] - lake[pool][0]
-    if pool == len(lake) + 1:
-        num+=(l-1)/L+1
+    l = lake[pool][1]-lake[pool][0]
+    if pool == len(lake)-1:
+        cnt +=  (l-1)//L +1
         break
-    if (l%L):
-        tmp = L -((l)%L)
-        now = lake[pool][1]+tmp
-        if lake[pool+1][0] <= now:
-            lake[pool+1][0] = now
-        num += (l)//L+1
-    else:
-        num += l/L
-print(int(num))
+    if (l) % L:
+        tmp = L - ((l)% L)
+        now_corver = lake[pool][1] + tmp
+        if lake[pool+1][0] <=now_corver :
+            lake[pool+1][0] = now_corver
+        cnt += (l)//L +1
+    else :
+        cnt += (l)//L
+print(cnt)
+#시간초과
+# for start, end in lake:
+#     if start > end:
+#         continue
+#     if now > start:
+#         start = now
+#     while start < end:
+#         start += L
+#         cnt += 1
+#     now = start
+# print(cnt)
