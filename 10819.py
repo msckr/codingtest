8f4f5bfc8fb74f279df6006ea3318b2a0bf53065
Minsoo Jo <mschoyb@naver.com>
2023-04-11 11:27:25 +0900

    10819 차이를 최대로

--- 10819.py
+++ 10819.py
@@ -1,5 +1,14 @@
+from itertools import permutations
+
 n = int(input())
 m = list(map(int,input().split()))
 m.sort()
-for _ in m:
-
+ans = 0
+per = permutations(m)
+for i in per:
+    s = 0
+    for j in range(len(i)-1):
+        s+= abs(i[j]-i[j+1])
+    if s > ans:
+        ans = s
+print(ans)
