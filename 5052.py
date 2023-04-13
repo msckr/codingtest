8b43003dac28763ad62f4f30ae74b9e23c5cc56e
Minsoo Jo <mschoyb@naver.com>
2023-04-13 19:05:57 +0900

    5052 전화번호 목록 풀이

--- 5052.py
+++ 5052.py
@@ -1,13 +1,17 @@
-t=int(input())
+import sys
+input = sys.stdin.readline
+
+t = int(input())
 for _ in range(t):
     n = int(input())
-    nums = [input() for _ in range(n)]
+    nums =[sys.stdin.readline().strip() for i in range(n)]
     nums.sort()
     check = True
     for i in range(n-1):
         felix = len(nums[i])
         if nums[i] == nums[i+1][:felix]:
             check = False
+            break
     if check:
         print("YES")
     else:
